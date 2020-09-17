a9d45e560932dfe82dd9d4f67ec4fa98998b044a
sebastian <sgaviria@uniguajira.edu.co>
2020-09-16 23:16:17 -0500

    clase cliente y clase banco

--- problema_3.py
+++ problema_3.py
@@ -11,9 +11,24 @@ class cliente():
     def devolver_cantidad(self):
         return self.cantidad
     def imprimir(self):
-        print(f"el cliente {self.nombre} tiene depositado en total: {self.cantidad}")  
+        print(f"el cliente {self.nombre} tiene depositado en total: {self.cantidad}") 
+
+class banco(cliente): 
+    def __init__(self):
+        self.cliente1 = cliente(input("digite el nombre del cliente: "))
+        self.cliente2 = cliente(input("digite el nombre del cliente: "))
+        self.cliente3 = cliente(input("digite el nombre del cliente: "))
+    def operacion_banco(self):
+        self.cliente1.depositar_cantidad(int(input("digite la cantidad a depositar: ")))
+        self.cliente1.extraer_cantidad(int(input("digite la cantidad a extraer: ")))
+        self.cliente2.depositar_cantidad(int(input("digite la cantidad a depositar: ")))
+        self.cliente2.extraer_cantidad(int(input("digite la cantidad a extraer: ")))
+        self.cliente3.depositar_cantidad(int(input("digite la cantidad a depositar: ")))
+        self.cliente3.extraer_cantidad(int(input("digite la cantidad a extraer: ")))
 def main():
     
+    banco1 = banco()
+    banco1.operacion_banco()
 
 
 
